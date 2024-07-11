481109de05253f430add924e483720873490de70
Juan Pablo <117771337+JuanPabloDV@users.noreply.github.com>
2024-07-10 23:43:10 -0300

    Finaliza seções "Footer" e "About"

--- src/components/layout/about/index.jsx
+++ src/components/layout/about/index.jsx
@@ -3,7 +3,12 @@ import styles from './index.module.css'
 export default function About() {
     return (
         <section className={styles.banner}>
-
+            <div>
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
+                <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
+                <div className={styles.line}/>
+                <p className={styles.about_date}>08.08.2021</p>
+            </div>
         </section>
     )
 }
